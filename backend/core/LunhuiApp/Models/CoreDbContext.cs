a8dac8271d46aebc315fbe3371ecac73f63eba09
Jia-algorithm <67299066+Jia-algorithm@users.noreply.github.com>
2021-07-13 15:18:24 +0800

    Add files via upload

---
+++ backend/core/LunhuiApp/Models/CoreDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLibrary.Models;
+
+namespace LunhuiApp.Models
+{
+    public class CoreDbContext : DbContext
+    {
+        public DbSet<Todos> Todos { get; set; }
+        public DbSet<Species> Species { get; set; }
+        public DbSet<Deads> Deads { get; set; }
+        public DbSet<Gongde> Gongde { get; set; }
+        public DbSet<Deads> Zhuanshengs { get; set; }
+        public CoreDbContext(DbContextOptions<CoreDbContext> options) : base(options)
+        {
+
+        }
+
+        public CoreDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // 添加组合主键
+            modelBuilder.Entity<Gongde>().HasKey(g => new { g.ObjID, g.eventID });
+        }
+    }
+}
